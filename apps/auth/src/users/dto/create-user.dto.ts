95d5a560156c3fe95feb68ce2f4b70b96cd450e0
rakeshcheekatimala <rakeshcheekatimala@gmail.com>
2024-04-30 17:40:10 +0800

    :feat: add auth project, users module, controller, service

---
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  @IsStrongPassword()
+  password: string;
+}
